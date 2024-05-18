51680c525e0208f2464efe1f7747e9e996e79f19
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-05-18 10:57:30 +0400

    changing columns names

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import pandas as pd
-
+import streamlit as st
 
 df = pd.read_csv('data_csv.csv')
+for i in df.columns:
+    if 'Unnamed:' in i:
+        print(i)
+        df.pop(i)
 
-print(df.to_string()) 
+# f = df.rename(columns={"Կարգին մարդ ": "writer",'Շատ տարածված արտահայտություն' : "common_phrase",'Տեքստ': 'Text',
+#                        'Հիմնական դերասաններ':"Main_Actors", 'հիմնական դերասանների քանակ': "Main_actors_count",
+#                        'Դերերի անուններ' : 'Role_names', 'Վայր' :'Place', 'Լուսավորվածություն': 'Lighting','Լեզուներ':"lang"
+#  })
+# f.to_csv("data_csv.csv") 
+print(df.columns)
