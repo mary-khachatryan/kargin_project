d4f390d6b49b0d0408f24d91d93789ce1821be42
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-05-18 23:06:39 +0400

    split names in main actors column

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-
+from itertools import chain
 df = pd.read_csv('data_csv.csv')
 for i in df.columns:
     if 'Unnamed:' in i:
@@ -12,4 +12,23 @@ for i in df.columns:
 #                        'Դերերի անուններ' : 'Role_names', 'Վայր' :'Place', 'Լուսավորվածություն': 'Lighting','Լեզուներ':"lang"
 #  })
 # f.to_csv("data_csv.csv") 
-print(df.columns)
+
+
+
+
+new_df = df.fillna(value = "None")
+all_names = []
+for i in new_df["Main_Actors"]:
+    for j in i.split(','):
+        
+        name = j.strip()
+        if " " in name:
+          lst = name.split(' ')
+          for k in lst:
+              all_names.append(k.strip(" "))
+        else:
+            all_names.append(name)
+print(all_names,type(all_names[0]))
+
+unique_names = []
+
