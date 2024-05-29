d1c508f82b90cc1866e077f2cfc9f6b3146241b5
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-05-29 14:26:04 +0400

    added  statistic function

--- app.py
+++ app.py
@@ -12,33 +12,37 @@ for i in df.columns:
 #                        'Դերերի անուններ' : 'Role_names', 'Վայր' :'Place', 'Լուսավորվածություն': 'Lighting','Լեզուներ':"lang"
 #  })
 # f.to_csv("data_csv.csv") 
-
-
-
-
 new_df = df.fillna(value = "None")
-all_names = []
-for i in new_df["Main_Actors"]:
-    for j in i.split(','):
-        
-        name = j.strip()
-        if " " in name:
-          lst = name.split(' ')
-          for k in lst:
-              all_names.append(k.strip(" "))
-        else:
-            all_names.append(name)
 
-unique_names = []
-unique_names_count ={}
-for i in all_names:
-    if i not in unique_names:
-        unique_names.append(i)
-        unique_names_count[i] = all_names.count(i)
+def statistic(column_name):
+    listof_data = []
+    for i in new_df[column_name]:
+        for j in i.split(','):
+            name = j.strip()
+            if " " in name:
+                lst = name.split(' ')
+                for k in lst:
+                    listof_data.append(k.strip(" "))
+            else:
+                listof_data.append(name)
+    unique_names = []
+    unique_names_count ={}
+    for i in listof_data:
+        if i not in unique_names:
+            unique_names.append(i)
+            unique_names_count[i] = listof_data.count(i)
         
 
-df_unique_names = pd.DataFrame(list(unique_names_count.items()), columns=['Name', 'Count'])
-df_unique_names = df_unique_names.sort_values(by = "Count",axis=0,ascending=False).head(20)
-print(df_unique_names,type(df_unique_names))
-fig = px.bar(df_unique_names, x='Name', y='Count', title='Count of Unique Names Top 20')
-st.plotly_chart(fig)
+    df_unique_names = pd.DataFrame(list(unique_names_count.items()), columns=['Name', 'Count'])
+    df_unique_names = df_unique_names.sort_values(by = "Count",axis=0,ascending=False).head(20)
+    print(df_unique_names,type(df_unique_names))
+    fig = px.bar(df_unique_names, x='Name', y='Count', title='Count of Unique Names Top 20')
+    st.plotly_chart(fig)
+
+
+statistic("Main_Actors")
+statistic("Place")
+statistic("Role_names")
+
+
+
